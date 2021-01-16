8c28d4a8a55477fadfda5ba287fe164fd5ae6e65
William <gopherx@protonmail.ch>
2021-01-16 11:53:41 +0800

    Update database schemas

--- pkg/database/schemas.go
+++ pkg/database/schemas.go
@@ -30,4 +30,26 @@ var migrations = []func(ctx context.Context, tx pgx.Tx) error{
 		_, err = tx.Exec(ctx, sql)
 		return err
 	},
+	func(ctx context.Context, tx pgx.Tx) (err error) {
+		sql := `
+			ALTER TABLE users
+			ADD COLUMN created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+			ADD COLUMN updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW();
+
+			CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+			RETURNS TRIGGER AS $$
+			BEGIN
+  				NEW.updated_at = NOW();
+  				RETURN NEW;
+			END;
+			$$ LANGUAGE plpgsql;
+
+			CREATE TRIGGER set_timestamp
+			BEFORE UPDATE ON users
+			FOR EACH ROW
+			EXECUTE PROCEDURE trigger_set_timestamp();
+		`
+		_, err = tx.Exec(ctx, sql)
+		return err
+	},
 }
